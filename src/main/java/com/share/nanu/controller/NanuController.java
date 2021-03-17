6e3d4813786ea5af2a83e092f45dacf315649851
castle1 <dlf5401@naver.com>
2021-03-17 18:12:33 +0900

    #master db test 회원리스트 출력

--- src/main/java/com/share/nanu/controller/NanuController.java
+++ src/main/java/com/share/nanu/controller/NanuController.java
@@ -1,5 +1,6 @@
 package com.share.nanu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.ModelAndView;
 import com.share.nanu.VO.MemberVO;
 import com.share.nanu.service.NanuService;
 
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 @Controller
 public class NanuController {
 	
+	@Autowired
 	private NanuService nservice;
 	
 	@GetMapping("/mem")
--- src/main/java/com/share/nanu/mapper/NanuMapper.java
+++ src/main/java/com/share/nanu/mapper/NanuMapper.java
@@ -2,8 +2,11 @@ package com.share.nanu.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.share.nanu.VO.MemberVO;
 
+@Mapper
 public interface NanuMapper {
 
 	public List<MemberVO> getlist(MemberVO mvo);
