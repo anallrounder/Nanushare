055a74e5d305fce35479dfe8192f7a70a575938b
castle1 <dlf5401@naver.com>
2021-03-22 11:52:29 +0900

    #master log4j 설정 완료 , console에 쿼리 출력 가능

--- src/main/java/com/share/nanu/controller/NanuController.java
+++ src/main/java/com/share/nanu/controller/NanuController.java
@@ -1,6 +1,5 @@
 package com.share.nanu.controller;
 
-import org.apache.logging.slf4j.SLF4JLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
