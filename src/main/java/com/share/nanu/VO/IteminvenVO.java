4b264ee1fbbb5beaff09279f068e45277897a332
DESKTOP-4FAT9VL\aiacademy <ktkt13134@gmail.com>
2021-03-24 11:29:17 +0900

    #dev_hj iteminvenVO 수정(조인)

--- src/main/java/com/share/nanu/VO/IteminvenVO.java
+++ src/main/java/com/share/nanu/VO/IteminvenVO.java
@@ -1,19 +1,26 @@
 package com.share.nanu.VO;
 
+import java.util.List;
+
+import lombok.ToString;
+@ToString
 public class IteminvenVO {
 	private int icat_num; // 물품 분류 번호
 	private String iname; // 물품명
 	private int iamount; // 물품 본사 재고수량
 
+	private List<ItemdonationVO> dona;
+
 	public IteminvenVO() {
 		super();
 	}
-
-	public IteminvenVO(int icat_num, String iname, int iamount) {
+	
+	public IteminvenVO(int icat_num, String iname, int iamount, List<ItemdonationVO> dona) {
 		super();
 		this.icat_num = icat_num;
 		this.iname = iname;
 		this.iamount = iamount;
+		this.dona = dona;
 	}
 
 	public int getIcat_num() {
@@ -38,6 +45,14 @@ public class IteminvenVO {
 
 	public void setIamount(int iamount) {
 		this.iamount = iamount;
+	}
+
+	public List<ItemdonationVO> getDona() {
+		return dona;
+	}
+
+	public void setDona(List<ItemdonationVO> dona) {
+		this.dona = dona;
 	}
 
 }
