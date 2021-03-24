98b0baf9da5f691739016026114090f1f412a493
castle1 <dlf5401@naver.com>
2021-03-24 12:21:46 +0900

    #master db수정 -> vo수정 VmamVO생성, VmVO 수정

--- src/main/java/com/share/nanu/VO/VmVO.java
+++ src/main/java/com/share/nanu/VO/VmVO.java
@@ -5,35 +5,21 @@ import lombok.ToString;
 @ToString
 public class VmVO {
 
-	private int vmindex; // 자판기인덱스
 	private String vm_num; // 자판기번호
 	private String lat; // 위도
 	private String lng; // 경도
 	private String branch; // 지점명
-	private int vmamount; // 잔여수량
-	private String iname; // 물품이름
 
 	public VmVO() {
 		super();
 	}
 
-	public VmVO(int vmindex, String vm_num, String lat, String lng, String branch, int vmamount, String iname) {
+	public VmVO(String vm_num, String lat, String lng, String branch) {
 		super();
-		this.vmindex = vmindex;
 		this.vm_num = vm_num;
 		this.lat = lat;
 		this.lng = lng;
 		this.branch = branch;
-		this.vmamount = vmamount;
-		this.iname = iname;
-	}
-
-	public int getVmindex() {
-		return vmindex;
-	}
-
-	public void setVmindex(int vmindex) {
-		this.vmindex = vmindex;
 	}
 
 	public String getVm_num() {
@@ -66,22 +52,6 @@ public class VmVO {
 
 	public void setBranch(String branch) {
 		this.branch = branch;
-	}
-
-	public int getVmamount() {
-		return vmamount;
-	}
-
-	public void setVmamount(int vmamount) {
-		this.vmamount = vmamount;
-	}
-
-	public String getIname() {
-		return iname;
-	}
-
-	public void setIname(String iname) {
-		this.iname = iname;
 	}
 
 }
---
+++ src/main/java/com/share/nanu/VO/Vmam.java
@@ -0,0 +1,54 @@
+package com.share.nanu.VO;
+
+public class Vmam {
+
+	private int vmindex; //vm index
+	private String vm_num; // 자판기 번호
+	private String vm_amount; // 자판기 수량
+	private String iname; // 물품이름
+
+	public Vmam() {
+		super();
+	}
+
+	public Vmam(int vmindex, String vm_num, String vm_amount, String iname) {
+		super();
+		this.vmindex = vmindex;
+		this.vm_num = vm_num;
+		this.vm_amount = vm_amount;
+		this.iname = iname;
+	}
+
+	public int getVmindex() {
+		return vmindex;
+	}
+
+	public void setVmindex(int vmindex) {
+		this.vmindex = vmindex;
+	}
+
+	public String getVm_num() {
+		return vm_num;
+	}
+
+	public void setVm_num(String vm_num) {
+		this.vm_num = vm_num;
+	}
+
+	public String getVm_amount() {
+		return vm_amount;
+	}
+
+	public void setVm_amount(String vm_amount) {
+		this.vm_amount = vm_amount;
+	}
+
+	public String getIname() {
+		return iname;
+	}
+
+	public void setIname(String iname) {
+		this.iname = iname;
+	}
+
+}
