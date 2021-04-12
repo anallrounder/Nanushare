bea4fed7c649872cacfa7f66c4b4a9256376454b
DESKTOP-P75P910\User <ktkt13134@gmail.com>
2021-04-12 10:20:26 +0900

    # 0412_hj 중간수정

--- src/main/java/com/share/nanu/controller/MyPageController.java
+++ src/main/java/com/share/nanu/controller/MyPageController.java
@@ -40,30 +40,27 @@ public class MyPageController {
 
 	@Autowired
 	private MemberService mservice;
-	
+
 	@Autowired
 	private NanuDonationService ndservice;
-	
+
 	@Autowired
 	BCryptPasswordEncoder encoder;
-	
-	
-	
 
 	// 마이페이지
 	@GetMapping("/my/mypage")
-	public ModelAndView myPage(MemberPointVO mpvo,MemberVO mvo, ModelAndView mav, @AuthenticationPrincipal MemberDetails md) {
+	public ModelAndView myPage(MemberPointVO mpvo, Criteria cri, MemberVO mvo, ModelAndView mav,
+			@AuthenticationPrincipal MemberDetails md) {
 
 		System.out.println("마이페이지");
-		
-		
-		
+
 		// 헤더에 로그인한 사람 정보 가져오기
 		if (md != null) { // 로그인을 해야만 md가 null이 아님, 일반회원, 관리자 ,소셜로그인 정상 적용
 			// log.info("로그인한 사람 이름 - "+ md.getmember().getName());
-			mav.addObject("username", md.getmember().getName()); 
+			mav.addObject("username", md.getmember().getName());
 		}
 		mav.addObject("memberInfo", ndservice.getMemberPoint(mpvo));
+		mav.addObject("list1", mgservice.myList1(cri, md.getUsername()));
 		mav.setViewName("/my/mypage");
 		return mav;
 	}
@@ -74,12 +71,11 @@ public class MyPageController {
 		if (md != null) {
 			mav.addObject("username", md.getmember().getName());
 		}
-		
-		//log.info(mvo.getMember_id().toString());
+
+		// log.info(mvo.getMember_id().toString());
 		mav.setViewName("/my/ask");
 		System.out.println("1.나의문의내역 +" + md.getUsername());
-		mav.addObject("list1", mgservice.myList1(cri,md.getUsername()));// 나의문의내역
-		
+		mav.addObject("list1", mgservice.myList1(cri, md.getUsername()));// 나의문의내역
 
 		// 페이징
 		int total = mgservice.getTotalCount1(cri, md.getUsername());
@@ -89,21 +85,20 @@ public class MyPageController {
 
 	// 2.나의인증내역
 	@GetMapping("/my/content")
-	public ModelAndView myCon( MemberVO mvo, ModelAndView mav, Criteria cri, @AuthenticationPrincipal MemberDetails md) {
+	public ModelAndView myCon(MemberVO mvo, ModelAndView mav, Criteria cri, @AuthenticationPrincipal MemberDetails md) {
 		if (md != null) {
 			mav.addObject("username", md.getmember().getName());
 		}
 		mav.setViewName("/my/content");
-		
-		//mgservice.mycontent(md.getUsername());
-		//mav.addObject("memberid", md.getUsername(mvo));
-		
+
+		// mgservice.mycontent(md.getUsername());
+		// mav.addObject("memberid", md.getUsername(mvo));
+
 		System.out.println("2.인증내역 +" + md.getUsername());
 		mav.addObject("list2", mgservice.myList2(cri, md.getUsername()));// 나의인증내역
-		
 
 		// 페이징
-		int total = mgservice.getTotalCount2(cri,md.getUsername());
+		int total = mgservice.getTotalCount2(cri, md.getUsername());
 		mav.addObject("pageMaker", new pageVO(cri, total));
 
 		return mav;
@@ -117,13 +112,13 @@ public class MyPageController {
 			mav.addObject("username", md.getmember().getName());
 		}
 		mav.setViewName("/my/give");
-		
-		//mav.addObject("mycon", md.getmember().getMember_id());//id만 넘겨줌
-		//mav.addObject("mycon", md.getUsername());
-		
-		mav.addObject("list3", mgservice.myList3(cri,md.getUsername()));// 나의나눔내역
+
+		// mav.addObject("mycon", md.getmember().getMember_id());//id만 넘겨줌
+		// mav.addObject("mycon", md.getUsername());
+
+		mav.addObject("list3", mgservice.myList3(cri, md.getUsername()));// 나의나눔내역
 		// 페이징
-		int total = mgservice.getTotalCount3(cri,md.getUsername());
+		int total = mgservice.getTotalCount3(cri, md.getUsername());
 		mav.addObject("pageMaker", new pageVO(cri, total));
 
 		return mav;
@@ -137,45 +132,47 @@ public class MyPageController {
 			mav.addObject("username", md.getmember().getName());
 		}
 		mav.setViewName("/my/reply");
-		mav.addObject("list4", mgservice.myList4(cri,md.getUsername()));// 나의문의내역
+		mav.addObject("list4", mgservice.myList4(cri, md.getUsername()));// 나의문의내역
 		// 페이징
-		int total = mgservice.getTotalCount4(cri,md.getUsername());
+		int total = mgservice.getTotalCount4(cri, md.getUsername());
 		mav.addObject("pageMaker", new pageVO(cri, total));
 		return mav;
 	}
-	
-	
+
 	// 5.나의결제내역
-		@GetMapping("/my/pay")
-		public ModelAndView myPay(MemberVO mvo, ModelAndView mav, Criteria cri,
-				@AuthenticationPrincipal MemberDetails md) {
-			if (md != null) {
-				mav.addObject("username", md.getmember().getName());
-			}
-			mav.setViewName("/my/pay");
-			mav.addObject("list5", mgservice.myList5(cri,md.getUsername()));// 나의문의내역
-			// 페이징
-			int total=mgservice.getTotalCount5(cri,md.getUsername());
-			
-			
-			
-			mav.addObject("pageMaker", new pageVO(cri, total));
-			return mav;
+	@GetMapping("/my/pay")
+	public ModelAndView myPay(MemberVO mvo, ModelAndView mav, Criteria cri, @AuthenticationPrincipal MemberDetails md) {
+		if (md != null) {
+			mav.addObject("username", md.getmember().getName());
 		}
+		mav.setViewName("/my/pay");
+		mav.addObject("list5", mgservice.myList5(cri, md.getUsername()));// 나의문의내역
+		// 페이징
+		int total = mgservice.getTotalCount5(cri, md.getUsername());
 
+		mav.addObject("pageMaker", new pageVO(cri, total));
+		return mav;
+	}
 
-	// https://melonpeach.tistory.com/49
 	// 프로필 관리로 가는 페이지(수정 전 비밀번호 확인 단계)
 	@GetMapping("/my/myprofile")
-	public ModelAndView myprofPw(ModelAndView mav) {
+	public ModelAndView myprofPw(ModelAndView mav, @AuthenticationPrincipal MemberDetails md) {
+		if (md != null) {
+			mav.addObject("username", md.getmember().getName());
+		}
+
 		log.info("프로필 페이지 비밀번호 확인 페이지 이동");
 		mav.setViewName("/my/myprofile");
 		return mav;
 	}
 
 	@PostMapping("/my/myprofile/check")
-	public ResponseEntity<String> myprofPw(@AuthenticationPrincipal MemberDetails md, @RequestBody String pwConfirm) {
+	public ResponseEntity<String> myprofPw(@AuthenticationPrincipal MemberDetails md, @RequestBody String pwConfirm,
+			ModelAndView mav) {
 
+		if (md != null) {
+			mav.addObject("username", md.getmember().getName());
+		}
 		ResponseEntity<String> entity = null;
 
 		try {
@@ -191,7 +188,7 @@ public class MyPageController {
 				entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
 			} else {
 				System.out.println("실패");
-				
+
 			}
 		} catch (Exception e) {
 			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
@@ -202,7 +199,10 @@ public class MyPageController {
 	}
 
 	@GetMapping("/my/myprofile/edit")
-	public ModelAndView myprofEdit(ModelAndView mav) {
+	public ModelAndView myprofEdit(@AuthenticationPrincipal MemberDetails md, ModelAndView mav) {
+		if (md != null) {
+			mav.addObject("username", md.getmember().getName());
+		}
 		log.info("수정 페이지로 이동");
 		mav.setViewName("/my/edit");
 
@@ -211,16 +211,18 @@ public class MyPageController {
 
 	// 수정
 	@PutMapping("/my/myprofile/edit/check")
-	public ResponseEntity<String> myprofEdit(@RequestBody MemberVO mvo) {
-		// public ResponseEntity<String> myprofileedit(@AuthenticationPrincipal
-		// MemberDetails md, @RequestBody MemberVO mvo) {
+	public ResponseEntity<String> myprofEdit(@RequestBody MemberVO mvo, @AuthenticationPrincipal MemberDetails md,
+			ModelAndView mav) {
+		if (md != null) {
+			mav.addObject("username", md.getmember().getName());
+		}
 		ResponseEntity<String> entity = null;
 
 		try {
 			mgservice.memberModifyPOST(mvo);
 			log.info("수정된 회원정보 : " + mvo);
 			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
-			// session.invalidate(); 세션날리고 다시 로그인해주세요 창띄우기
+			// session.invalidate();
 		} catch (Exception e) {
 			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
 			e.printStackTrace();
@@ -229,7 +231,10 @@ public class MyPageController {
 	}
 
 	@GetMapping("/my/drop")
-	public ModelAndView myprofDel(ModelAndView mav) {
+	public ModelAndView myprofDel(ModelAndView mav, @AuthenticationPrincipal MemberDetails md) {
+		if (md != null) {
+			mav.addObject("username", md.getmember().getName());
+		}
 		log.info("탈퇴 페이지로 이동");
 		mav.setViewName("/my/drop");
 		return mav;
@@ -237,7 +242,12 @@ public class MyPageController {
 
 	// 탈퇴
 	@PostMapping("/my/drop/check")
-	public ResponseEntity<String> myprofDel(@AuthenticationPrincipal MemberDetails md, @RequestBody MemberVO mvo) {
+	public ResponseEntity<String> myprofDel(ModelAndView mav, @AuthenticationPrincipal MemberDetails md,
+			@RequestBody MemberVO mvo) {
+		if (md != null) {
+			mav.addObject("username", md.getmember().getName());
+		}
+
 		ResponseEntity<String> entity = null;
 		System.out.println("시험" + mvo);
 		log.info("탈퇴체크페이지");
@@ -254,40 +264,41 @@ public class MyPageController {
 		return entity;
 	}
 
-	
 	@GetMapping("/deleteCooki")
-	public void deleteCooki(ModelAndView mav, HttpServletResponse response, HttpServletRequest request
-			,@CookieValue(value = "Nanushare_cooki", required = false ) Cookie Nanushare,
+	public void deleteCooki(@AuthenticationPrincipal MemberDetails md, ModelAndView mav, HttpServletResponse response,
+			HttpServletRequest request, @CookieValue(value = "Nanushare_cooki", required = false) Cookie Nanushare,
 			@CookieValue(value = "JSESSIONID", required = false) Cookie JSESSIONID) throws IOException {
+		if (md != null) {
+			mav.addObject("username", md.getmember().getName());
+		}
+
 		log.info("쿠키 삭제");
 		// remember-me 쿠키인 Nanushare_cooki, 톰캣 쿠키인 JSESSIONID을제거
-		//새로고침이 필요하기 때문에 리다이렉트로 전달
-		
-		String redirect_url = "/main"; //리다이렉트로 메인으로 이동할 주소
-		
-		if(Nanushare != null && JSESSIONID !=null ) { //널체크
+		// 새로고침이 필요하기 때문에 리다이렉트로 전달
+
+		String redirect_url = "/main"; // 리다이렉트로 메인으로 이동할 주소
+
+		if (Nanushare != null && JSESSIONID != null) { // 널체크
 			System.out.println(Nanushare.getName());
 			System.out.println(Nanushare.getValue());
 			System.out.println(JSESSIONID.getName());
 			System.out.println(JSESSIONID.getValue());
-			
+
 			JSESSIONID.setMaxAge(0);
 			JSESSIONID.setSecure(true);
 			JSESSIONID.setHttpOnly(true);
 			JSESSIONID.setPath("/");
 			response.addCookie(JSESSIONID);
-			
-			
+
 			Nanushare.setMaxAge(0);
 			Nanushare.setSecure(true);
 			Nanushare.setHttpOnly(true);
 			Nanushare.setPath("/");
 			response.addCookie(Nanushare);
 		}
-		
-		
-		response.sendRedirect(redirect_url); //restcontroller는 response를 사용하여야 한다.
-		
+
+		response.sendRedirect(redirect_url); // restcontroller는 response를 사용하여야 한다.
+
 	}
 
 }
