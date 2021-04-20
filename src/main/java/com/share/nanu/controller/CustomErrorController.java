ad297314e95961fefc3e863221bce1018be6ac9c
castle1 <dlf5401@naver.com>
2021-04-20 15:35:32 +0900

    #dev_si_questionBoard 주석 추가

--- src/main/java/com/share/nanu/controller/CustomErrorController.java
+++ src/main/java/com/share/nanu/controller/CustomErrorController.java
@@ -1,10 +1,8 @@
 package com.share.nanu.controller;
 
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
@@ -14,38 +12,35 @@ import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
 
-
-
 @Slf4j
 @Controller
-public class CustomErrorController implements ErrorController{
-	
+public class CustomErrorController implements ErrorController {
+
 	@GetMapping("/errorPage")
 	public ModelAndView handlerError(ModelAndView mav, HttpServletRequest request, HttpServletResponse response) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		log.info("에러 코드 : " + status);
-		
-		if(status != null) {
+
+		if (status != null) {
 			Integer statusCode = Integer.valueOf(status.toString());
-			if(statusCode == HttpStatus.NOT_FOUND.value()) {
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
 				mav.setViewName("/error/404");
-			}else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
 				mav.setViewName("/error/404");
-			}else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+			} else if (statusCode == HttpStatus.FORBIDDEN.value()) {
 				mav.setViewName("/error/404");
-			}else {
+			} else {
 				mav.setViewName("/error/404");
 			}
 		}
-		
-		
+
 		return mav;
-		
+
 	}
-	
+
 	@Override
 	public String getErrorPath() {
-		
+
 		return null;
 	}
 
