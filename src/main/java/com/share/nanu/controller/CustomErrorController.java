c3b85803e16c28c4ef98b1a6ff96d2b525bac601
castle1 <dlf5401@naver.com>
2021-04-20 15:21:33 +0900

    #dev_si_questionBoard 에러발생시 -> 커스텀 에러페이지로 이동 완료 돌아가기 주소만 수정해야함

--- src/main/java/com/share/nanu/controller/CustomErrorController.java
+++ src/main/java/com/share/nanu/controller/CustomErrorController.java
@@ -1,27 +1,48 @@
 package com.share.nanu.controller;
 
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
 
 
 
+@Slf4j
 @Controller
-@RequestMapping("/error")
 public class CustomErrorController implements ErrorController{
 	
-	private String path = "/error";
-	private String viewPath = "404";
+	@GetMapping("/errorPage")
+	public ModelAndView handlerError(ModelAndView mav, HttpServletRequest request, HttpServletResponse response) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		log.info("에러 코드 : " + status);
+		
+		if(status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+			if(statusCode == HttpStatus.NOT_FOUND.value()) {
+				mav.setViewName("/error/404");
+			}else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				mav.setViewName("/error/404");
+			}else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				mav.setViewName("/error/404");
+			}else {
+				mav.setViewName("/error/404");
+			}
+		}
+		
+		
+		return mav;
+		
+	}
 	
-	/*
-	 * @RequestMapping("/error") public String error() {
-	 * 
-	 * }
-	 */
-	
-
 	@Override
 	public String getErrorPath() {
 		
