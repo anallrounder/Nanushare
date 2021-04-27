5eead97daeb78d172262c699d853c6b212dbb1f4
castle1 <dlf5401@naver.com>
2021-04-27 15:19:24 +0900

    #컨트롤러 mock 테스트

---
+++ src/test/java/com/share/nanu/test/mockControllerTest.java
@@ -0,0 +1,30 @@
+package com.share.nanu.test;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class mockControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	
+	@Test
+	public void signUpFormTest() throws Exception {
+		
+		mockMvc.perform(MockMvcRequestBuilders.get("/loginPage").accept(MediaType.TEXT_HTML))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andDo(MockMvcResultHandlers.print());
+	}
+
+}
